7503452bdffb992f6c78dec756d99e80afc08eab
Andrey Goryushin <goryushin.andre@yandex.ru>
2018-02-20 19:44:47 +0300

    version 1.0.2

--- src/blocks/Layout/index.jsx
+++ src/blocks/Layout/index.jsx
@@ -6,14 +6,69 @@ class Layout extends Component {
         return (
             <div className="layout">
                 <div className="header">
-                    
+                    <div className="container-fluid">
+                        <div className="row justify-content-between align-items-center">
+                            <div className="col-md-auto col-sm-8">
+                                <div className="header_desc">
+                                    <p className="header_desc-logo">ЛОГО</p>
+                                    <p>Информация о товаре и магазине</p>
+                                    <a href="">8 (812) 532-46-28</a> <br/>
+                                    <span>c 9:00 до 19:00 по будням</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 
                 <div className="content">
-                    {this.props.children}
+                    <div className="container">
+                        <div className="row justify-content-center align-items-center">
+                            <div className="content_button">
+                                <button type="submit" className="btn btn-primary">Регистрация</button>
+                                <button type="submit" className="btn btn-primary">Войти</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="footer">
-
+                    <div className="container">
+                        <div className="row justify-content-around">
+                            <div className="col-md-2">
+                                <h4>Покупатели</h4>
+                                <ul>
+                                    <li><a href="#">О нас</a></li>
+                                    <li><a href="#">Доставка и оплата</a></li>
+                                    <li><a href="#">Возврат/Обмен</a></li>
+                                    <li><a href="#">Контакты</a></li>
+                                    <li><a href="#">Идеи подарков</a></li>
+                                    <li><a href="#">Календарь праздников</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-auto">
+                                <h4>Личный кабинет</h4>
+                                <ul>
+                                    <li><a href="#">Вход</a></li>
+                                    <li><a href="#">Регистрация</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-auto">
+                            <h4>Документы</h4>
+                                <ul>
+                                    <li><a href="#">Пользовательское соглашение</a></li>
+                                    <li><a href="#">Политика конфиденциальности</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-auto">
+                                <h4>Расскажи о нас <br/> в соц.сетях</h4>
+                                <ul className="footer_soc-icon">
+                                    <li><a href="#"><i className="fa fa-vk"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
